3824109f0b809dac9603eb3b48ece041ff6bd8b2
ScottInPittsburgh <scottdrickman27@gmail.com>
2024-06-04 20:26:41 -0400

    Initial commit test on NetBeans 22.  Java with Maven.

---
+++ src/main/java/com/mycompany/ist412_group5/IST412_Group5.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ist412_group5;
+
+/**
+ *
+ * @author scottst.clair
+ */
+public class IST412_Group5 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
