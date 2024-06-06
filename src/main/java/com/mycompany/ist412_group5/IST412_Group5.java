054f62f0fc748a1fe97fba28baee2ba7cc8512fb
ScottInPittsburgh <scottdrickman27@gmail.com>
2024-06-05 20:54:46 -0400

    Added new packages for MVC model and test classes/methods.  Tested in main.

--- src/main/java/com/mycompany/ist412_group5/IST412_Group5.java
+++ src/main/java/com/mycompany/ist412_group5/IST412_Group5.java
@@ -1,16 +1,38 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.ist412_group5;
 
-/**
- *
- * @author scottst.clair
- */
-public class IST412_Group5 {
+import com.mycompany.ist412_group5.controller.SchedulingController;
+import com.mycompany.ist412_group5.controller.StatisticsController;
+import com.mycompany.ist412_group5.controller.TicketController;
+import com.mycompany.ist412_group5.controller.UserProfileController;
+import com.mycompany.ist412_group5.view.SchedulingView;
+import com.mycompany.ist412_group5.view.StatisticsView;
+import com.mycompany.ist412_group5.view.TicketView;
+import com.mycompany.ist412_group5.view.UserProfileView;
 
+public class IST412_Group5 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+        SchedulingController schedulingController = new SchedulingController();
+        StatisticsController statisticsController = new StatisticsController();
+        TicketController ticketController = new TicketController();
+        UserProfileController userProfileController = new UserProfileController();
+
+        SchedulingView schedulingView = new SchedulingView();
+        StatisticsView statisticsView = new StatisticsView();
+        TicketView ticketView = new TicketView();
+        UserProfileView userProfileView = new UserProfileView();
+
+        
+        schedulingController.exampleSchedulingMethod();
+        schedulingView.showActivities();
+
+        statisticsController.exampleStatisticsMethod();
+        statisticsView.showStatistics();
+
+        ticketController.exampleTicketMethod();
+        ticketView.showTickets();
+
+        userProfileController.exampleUserProfileMethod();
+        userProfileView.showUserProfile();
     }
 }
