0bda9cc3e498af97955aa79230a97b37e5089888
Bright Darko <58749600+bdarko1125@users.noreply.github.com>
2024-06-06 23:04:20 +0000

    Initial commit

--- src/main/java/com/mycompany/ist412_group5/view/StatisticsView.java
+++ src/main/java/com/mycompany/ist412_group5/view/StatisticsView.java
@@ -1,8 +1,25 @@
 package com.mycompany.ist412_group5.view;
 
+/**
+ * View for displaying statistics-related information.
+ */
 public class StatisticsView {
-    public void showStatistics() {
-        System.out.println("Displaying statistics...");
+
+    private StatisticsController statisticsController;
+
+    public StatisticsView() {
+        this.statisticsController = new StatisticsController();
+    }
+
+    public void displayRealTimeStatistics() {
+        ParkStatistics stats = statisticsController.getRealTimeStatistics();
+        System.out.println("Attendance: " + stats.getAttendance());
+        System.out.println("Ride Wait Times: " + stats.getRideWaitTimes());
+        System.out.println("User Feedback: " + stats.getUserFeedback());
+    }
+
+    public void displayReport() {
+        Report report = statisticsController.generateReport();
+        System.out.println("Report Content: " + report.getContent());
     }
 }
-
