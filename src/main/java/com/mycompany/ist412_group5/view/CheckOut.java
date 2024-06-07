d7a279e4384fd717c47894c7483806ca5194da55
JedCypher <121827213+JedCypher@users.noreply.github.com>
2024-06-06 20:05:02 -0400

    Add files via upload

---
+++ src/main/java/com/mycompany/ist412_group5/view/CheckOut.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package TicketMaster;
+
+/**
+ *
+ * @author Jed Elijah Galvo
+ */
+public class CheckOut extends Cart{
+    
+    //variables for checkout class
+    String payment = " ";
+    
+    //This method allows the user to see the number of tickets purchase, the purchase price (total) and the tax.
+ public void add(int price, int tax, int Ticket_Purchase){}   
+}
+
+
