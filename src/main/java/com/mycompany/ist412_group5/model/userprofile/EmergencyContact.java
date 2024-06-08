08f7955ab96e5946b0f031508074ee5a7268acf2
fdi12 <fdi1216@gmail.com>
2024-06-08 17:27:05 -0400

    updating Javadoc comments

--- src/main/java/com/mycompany/ist412_group5/model/userprofile/EmergencyContact.java
+++ src/main/java/com/mycompany/ist412_group5/model/userprofile/EmergencyContact.java
@@ -10,18 +10,39 @@ public class EmergencyContact {
     private String name;
     private String phone;
 
+    /**
+     * Gets the name of the emergency contact.
+     *
+     * @return the name of the emergency contact
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Sets the name of the emergency contact.
+     *
+     * @param name the new name of the emergency contact
+     */
     public void setName(String name) {
         this.name = name;
     }
+
+    /**
+     * Gets the phone number of the emergency contact.
+     *
+     * @return the phone number of the emergency contact
+     */
 
     public String getPhone() {
         return phone;
     }
 
+    /**
+     * Sets the phone number of the emergency contact.
+     *
+     * @param phone the new phone number of the emergency contact
+     */
     public void setPhone(String phone) {
         this.phone = phone;
     }
